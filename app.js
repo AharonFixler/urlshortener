e5c05d4935b02580040711ce2a95baa7499a2fea
AharonFixler <ariandleah@gmail.com>
2025-01-14 12:44:44 +0200

    Update app.js
    change to '

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { shortenerRoutes } from "./routes/shortener.routes.js";
+import express from 'express';
+import { shortenerRoutes } from './routes/shortener.routes.js';
 
 const app = express();
 
