6b258aa41bace1ec9d0123a6478544eb157ea11a
AharonFixler <ariandleah@gmail.com>
2025-01-14 13:04:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 import express from 'express';
-import { shortenerRoutes } from './routes/shortener.routes.js';
+//import { shortenerRoutes } from './routes/shortener.routes.js';
 
-const app = express();
+//const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.static("public"));
+//app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs");
+//app.set("view engine", "ejs");
 // app.set("views", "./views")
 
 //? In Express.js, a template engine is a tool that lets you embed dynamic content into HTML files and render them on the server before sending them to the client. It allows you to create reusable templates, making it easier to generate dynamic web pages with minimal code.
 
 // express router
 // app.use(router);
-app.use(shortenerRoutes);
+//app.use(shortenerRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+//app.listen(PORT, () => {
+//  console.log(`Server running at http://localhost:${PORT}`);
+//});
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(PORT);
